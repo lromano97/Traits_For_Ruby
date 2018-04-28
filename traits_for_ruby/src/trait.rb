9c7ab8439a98e8e84c135ee6b6755ede366e0d8a
Gustavo Ayrton Bitocchi <Gusbitocchi97@gmail.com>
2018-04-27 22:29:25 -0300

    Te quiero lucki

--- traits_for_ruby/src/trait.rb
+++ traits_for_ruby/src/trait.rb
@@ -1,37 +1,56 @@
-class Class
-  def uses(a_module)
-    include(a_module)
+class Module
+  def +(trait)
+    unModulo = Module.new
+
+    mis_metodos = instance_methods(false)
+    sus_metodos = trait.instance_methods(false)
+
+    conflictivos = mis_metodos & sus_metodos
+    sus_metodos -= conflictivos
+    sus_metodos.concat mis_metodos
+
+    for i in 0...sus_metodos.length
+      unModulo.send(:define_method,sus_metodos[i].to_s) do
+          if(conflictivos.include? sus_metodos[i])
+            raise 'Metodos conflictivos'
+          elsif
+            trait.method(sus_metodos[i]).call
+          end
+      end
+    end
+
+    unModulo
   end
 end
 
-class Module
-  def +(a_module)
-    my_methods = instance_methods(false)
-    its_methods = a_module.instance_methods(false)
+class Class
 
-    conflict_methods = my_methods & its_methods
-
-    newModule = Module.new
-
-    for i in 0...its_methods.length
-      newModule.send(:define_method, its_methods[i]) do
-        a_module.method(its_methods[i]).call
-      end
-    end
-    #Redefinir los metodos con Raise error
+  def uses(_unModulo)
+    include(_unModulo)
   end
+
 end
 
 class Trait
   def self.define(name, methods_name, behaviour)
     Object.const_set name, Module.new
+    if(methods_name!=nil)
     for i in 0...methods_name.length
-      Kernel.const_get(name).send(:define_method, methods_name[i],
-                                  &behaviour[i])
+      Kernel.const_get(name).send(:define_method,methods_name[i],&behaviour[i])
+    end
     end
   end
 end
 
+name = :I
+Trait.define name, [:w,:r],[proc {puts "xdd"},proc {puts "ivan schedule"}]
+
+name2 = :J
+Trait.define name2, [:p,:r],[proc {puts "xdd"},proc {puts "ivan schedule"}]
 
 
+class XD uses I + J
+end
 
+hiban = XD.new()
+puts hiban.r
