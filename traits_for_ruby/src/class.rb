f13c050fe703a1821449bcd65be909b416ff59aa
Gustavo Ayrton Bitocchi <Gusbitocchi97@gmail.com>
2018-05-07 01:31:45 -0300

    Update class.rb

--- traits_for_ruby/src/class.rb
+++ traits_for_ruby/src/class.rb
@@ -1,8 +1,40 @@
-class Class
+require_relative 'strategy'
 
-  def uses(_unModulo)
-    include(_unModulo)
+class Class
+  def strategy(strategy_hash)
+    const_set :Strategy, strategy_hash
   end
 
+  def uses(a_module)
+    include Strategy
+    un_modulo = Module.new
+    if const_get(:Strategy) == Strategy
+      method = instance_method(:default_strategy)
+      a_module.const_get(:Methods).each do |method_name, block|
+        if block.is_a? Array
+          method.bind(self).call un_modulo,method_name,block
+        end
+      end
+    else
+      const_get(:Strategy).each do |key,value|
+        a_module.const_get(:Methods).each do |method_name, block|
+        if value.include? method_name
+          if key.is_a? Proc
+            key.call un_modulo, method_name, block
+          elsif
+            key.is_a? Array
+            method = instance_method(key[0])
+            method.bind(self).call key[1],un_modulo,method_name,block
+          else
+            method = instance_method(key)
+            method.bind(self).call un_modulo, method_name, block
+          end
+        else
+          send(:define_method,method_name,block) unless block.is_a? Array
+        end
+        end
+      end
+    end
+    include un_modulo
+  end
 end
-
