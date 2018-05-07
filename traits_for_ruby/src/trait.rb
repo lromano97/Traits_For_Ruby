3a151b1fe3ff3311f577ea5e01d82831061a9c08
Lucas Romano <lromano@est.frba.utn.edu.ar>
2018-05-07 01:32:14 -0300

    Modifico el funcionamiento de trait

--- traits_for_ruby/src/trait.rb
+++ traits_for_ruby/src/trait.rb
@@ -1,15 +1,15 @@
 require_relative 'class'
 require_relative 'algebra'
+require_relative 'strategy'
 require_relative 'symbol'
 
-
 class Trait
-  def self.define(name, method_map_hash)
+  def self.define(name, methods)
     Object.const_set name, Module.new
     Kernel.const_get(name).singleton_class.include(Algebra)
-    methods_key = method_map_hash.keys
-    methods_key.each do |method|
-      Kernel.const_get(name).send(:define_method, method, method_map_hash[method])
+    Kernel.const_get(name).const_set :Methods, Hash.new
+    methods.each do |method_name, block|
+      Kernel.const_get(name).const_get(:Methods)[method_name] = block
     end
   end
 end
